f4f4a5300a34e5741a79e4db39e1dddeab0ad269
JessicaDMunozO <jessitae@gmail.com>
2023-09-21 12:15:26 -0500

    Métodos Set y correcciones

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -5,8 +5,8 @@
  */
 package edu.eci.arsw.blueprints.services;
 
-
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintFilter;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -31,22 +31,22 @@ public class BlueprintsServices {
 
     @Autowired
     @Qualifier("Redundancy")
-    BlueprintFilter blueprintFilter=null;
+    BlueprintFilter blueprintFilter = null;
 
-
-    public void addNewBlueprint(Blueprint bp) {
-        try {
-            Blueprint blueprint=blueprintFilter.filter(bp);
-            bpp.saveBlueprint(blueprint);
-        } catch (BlueprintPersistenceException e) {
-        }
+    public void addNewBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        Blueprint blueprint = blueprintFilter.filter(bp);
+        bpp.saveBlueprint(blueprint);
     }
 
     public Set<Blueprint> getAllBlueprints() {
-        Set<Blueprint> blueprints= bpp.getAllBlueprints();
+        Set<Blueprint> blueprints = bpp.getAllBlueprints();
         bpp.deleteBlueprints();
-        for (Blueprint bp: blueprints){
-            addNewBlueprint(bp);
+        for (Blueprint bp : blueprints) {
+            try {
+                addNewBlueprint(bp);
+            } catch (BlueprintPersistenceException e) {
+                e.printStackTrace();
+            }
         }
         return bpp.getAllBlueprints();
     }
@@ -60,10 +60,14 @@ public class BlueprintsServices {
      */
     public Blueprint getBlueprint(String author, String name) throws BlueprintNotFoundException {
         try {
-            Set<Blueprint> blueprints= bpp.getAllBlueprints();
+            Set<Blueprint> blueprints = bpp.getAllBlueprints();
             bpp.deleteBlueprints();
-            for (Blueprint bp: blueprints){
-                addNewBlueprint(bp);
+            for (Blueprint bp : blueprints) {
+                try {
+                    addNewBlueprint(bp);
+                } catch (BlueprintPersistenceException e) {
+                    e.printStackTrace();
+                }
             }
             return bpp.getBlueprint(author, name);
         } catch (BlueprintNotFoundException e) {
@@ -79,10 +83,14 @@ public class BlueprintsServices {
      */
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
         try {
-            Set<Blueprint> blueprints= bpp.getAllBlueprints();
+            Set<Blueprint> blueprints = bpp.getAllBlueprints();
             bpp.deleteBlueprints();
-            for (Blueprint bp: blueprints){
-                addNewBlueprint(bp);
+            for (Blueprint bp : blueprints) {
+                try {
+                    addNewBlueprint(bp);
+                } catch (BlueprintPersistenceException e) {
+                    e.printStackTrace();
+                }
             }
             return bpp.getBlueprintByAuthor(author);
         } catch (BlueprintNotFoundException e) {
@@ -90,5 +98,23 @@ public class BlueprintsServices {
         }
     }
 
+    private void setNameBlueprint(Blueprint blueprint, String bpname) {
+        blueprint.setName(bpname);
+    }
+
+    private void setAuthorBlueprint(Blueprint blueprint, String author) {
+        blueprint.setAuthor(author);
+    }
+
+    private void setPointsBlueprint(Blueprint blueprint, List<Point> points) {
+        blueprint.setPoints(points);
+    }
+
+    public void setBlueprint(Blueprint blueprint, String author, String bpname) throws BlueprintNotFoundException {
+        Blueprint bp = getBlueprint(author, bpname);
+        setNameBlueprint(bp, blueprint.getName());
+        setAuthorBlueprint(bp, blueprint.getAuthor());
+        setPointsBlueprint(bp, blueprint.getPoints());
+    }
 
 }
