678c932e21e3a924c9c4ee7147db6842247d5107
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-09-21 15:54:15 -0500

    Rollback

--- src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -1,10 +1,8 @@
 package edu.eci.arsw.blueprints.test.services;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootConfiguration
 @SpringBootTest()
 public class ApplicationServicesTests {
 
